4f26d9a1efdb589220e17ddd67eb2656f5f2ba1b
Sibanye <sibanyebukani01@gmail.com>
2020-04-06 20:03:07 +0200

    index page

--- includes/indexfooter.php
+++ includes/indexfooter.php
@@ -1,61 +1,9 @@
-
-<footer>
-    <div class="text-center small ">
-        <p class="copyright">
-            <?php
-    ini_set('date.timezone', 'Africa/Harare');
-    $startYear = 2020;
-    $thisYear = date('Y');
-    if ($startYear == $thisYear) {
-      echo $startYear;
-    } else {
-      echo "{$startYear}-{$thisYear}";
-    }
-    ?>
-            &copy; BukaniTech Solutions ----- All Rights Reserved</p>
-        <div>
-        </div>
-    </div>
-</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <!-- footer date script-->
-
-
-<script>
-$(document).ready(function() {
-
-    $('.button-psswd').on('click', function() {
-
-        if ($('.input-psswd').attr('psswd-shown') == 'false') {
-
-            $('.input-psswd').removeAttr('type');
-            $('.input-psswd').attr('type', 'text');
-
-            $('.input-psswd').removeAttr('psswd-shown');
-            $('.input-psswd').attr('psswd-shown', 'true');
-
-            $('.button-psswd').html('Hide');
-
-        } else {
-
-            $('.input-psswd').removeAttr('type');
-            $('.input-psswd').attr('type', 'password');
-
-            $('.input-psswd').removeAttr('psswd-shown');
-            $('.input-psswd').attr('psswd-shown', 'false');
-
-            $('.button-psswd').html('Show');
-
-        }
-
-    });
-
-});
-</script>
 
 
 </body>
--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php $page_title = 'PSA Houses - Home '; ?>
 <?php include('includes/indexheader.php'); ?>
-<?php
-if (!Login::isLoggedIn()) {
-  Redirect::redirect_to("registration/login.php");
-  array_push($errors, "Please Login First");
-}
-?>
 
-<a href='registration/logout.php' class='login-links'>Logout</a><br><br>
+    <section class="container login ">
+    <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <?php echo display_error(); ?>
+            <?php echo display_success(); ?>
+            <p class="p-0 m-0" style="color:white">Online <span>Student Accommodation</span> Community</p>
+            <h1 class="site-title">P<span>S</span>A</h1>
+            <p style="color:white">Welcome to Private <span>Student</span> Accommodation Houses</p>
+            <a href="community.php"><button class="land-button">Community</button></a>
+            <a href="registrarion/register.php"><button class="land-button">Register</button></a>
+            <a href="indexabout.php"><button class="land-button">About PSA Houses</button></a>
+        </div>
+    </div>
+        
+    </section>
+
 
 
 
